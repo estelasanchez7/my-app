a907c992b3f72c81acefa3296c781751b703f27b
estelasanchez7 <stelasv7@gmail.com>
2023-09-11 10:59:00 +0200

    angular desde 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgOptimizedImage } from '@angular/common';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'changed';
+  }
+
+  ngOnInit() {
+    /* esto es lo mismo que useEffect en React */
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => { 
+        this.products = data;
+      });
+  }
 }
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,7 @@
+/*Atributos esperados de un producto*/
+export interface Product{
+    id: number;
+    title: string;
+    price: number;
+    images: string[];
+}
